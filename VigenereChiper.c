9656946504b1b7e9e4844b5d711b304162ebcb02
ninejuan <ninejuan@protonmail.com>
2024-06-11 09:54:29 +0900

    add: Encrypt Decrypt 선택

--- VigenereChiper.c
+++ VigenereChiper.c
@@ -16,14 +16,6 @@ void changeConsoleColor(unsigned color) {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 
-char encrypt() {
-
-}
-
-char decrypt() {
-
-}
-
 int main() {
 	init();
 	changeConsoleColor(yellow);
@@ -39,22 +31,5 @@ int main() {
 	changeConsoleColor(yellow);
 	printf("=================================================================================================================================\n");
 	changeConsoleColor(white); printf("\n");
-	printf("옵션을 선택하세요. (숫자만 입력)\n\n");
-	printf("1. 암호화");
-	printf("2. 복호화");
-	int option; scanf_s("%d", &option);
-	char res;
-	switch (option) {
-	case 1:
-		res = encrypt();
-		break;
-	case 2:
-		res = decrypt();
-		break;
-	default:
-		printf("정확한 값을 입력해주세요");
-		exit(0);
-		break;
-	}
-	return 0;
+	
 }
